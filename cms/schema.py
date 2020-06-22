6be0126111d109e046c78ae7adcbc341edff9712
Guillermo Crescencio <guillermo.crescencio@laureate.net>
2020-06-21 22:44:43 -0600

    Stream fields

--- cms/schema.py
+++ cms/schema.py
@@ -1,16 +1,99 @@
+import string
 import graphene
 
-from graphene_django.types import DjangoObjectType
+from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
 
-from home.models import HomePage
+from graphene.types import Scalar
+from graphene.types.generic import GenericScalar
+from graphene_django.types import DjangoObjectType
+from graphene_django.converter import convert_django_field
+
+from home.models import HomePage, Recipe
+
+
+class GenericStreamFieldType(Scalar):
+    @staticmethod
+    def serialize(stream_value):
+        return stream_value.stream_data
+
+
+@convert_django_field.register(StreamField)
+def convert_stream_field(field, registry=None):
+    return GenericStreamFieldType(
+        description=field.help_text, required=not field.null
+    )
+
+
+class DefaultStreamBlock(graphene.ObjectType):
+    block_type = graphene.String()
+    value = GenericScalar()
+
+
+def create_stream_field_type(field_name, **kwargs):
+    block_type_handlers = kwargs.copy()
+
+    class Meta:
+        types = (DefaultStreamBlock, ) + tuple(
+            block_type_handlers.values())
+
+    StreamFieldType = type(
+        f"{string.capwords(field_name, sep='_').replace('_', '')}Type",
+        (graphene.Union,),
+        dict(Meta=Meta))
+
+    def convert_block(block):
+        block_type = block.get('type')
+        value = block.get('value')
+        if block_type in block_type_handlers:
+            handler = block_type_handlers.get(block_type)
+            if isinstance(value, dict):
+                return handler(value=value, block_type=block_type, **value)
+            else:
+                return handler(value=value, block_type=block_type)
+        else:
+            return DefaultStreamBlock(value=value, block_type=block_type)
+
+    # We also generate the resolver function for the field
+    def resolve_field(self, info):
+        field = getattr(self, field_name)
+        return [convert_block(block) for block in field.stream_data]
+
+    return (graphene.List(StreamFieldType), resolve_field)
+
+
+class RecipeNode(DjangoObjectType):
+    class Meta:
+        model = Recipe
+
+
+class ParagraphBlock(DefaultStreamBlock):
+    value = GenericScalar()
+
+
+class HeadingBlock(DefaultStreamBlock):
+    value = GenericScalar()
+
+
+class RecipeBlock(DefaultStreamBlock):
+    recipe = graphene.Field(RecipeNode)
+
+    def resolve_recipe(self, info):
+        return Recipe.objects.get(id=self.value)
 
 
 class HomeType(DjangoObjectType):
+    (stream, resolve_stream) = create_stream_field_type(
+        'stream',
+        paragraph=ParagraphBlock,
+        heading=HeadingBlock,
+        recipe=RecipeBlock)
+
     class Meta:
         model = HomePage
 
 
-class HomeQuery(object):
+class HomeQuery():
     all_home = graphene.List(HomeType)
 
     home = graphene.Field(
@@ -18,7 +101,7 @@ class HomeQuery(object):
         id=graphene.Int(),
         slug=graphene.String())
 
-    child_of = graphene.Field(
+    child_of = graphene.List(
         HomeType,
         id=graphene.Int(),
         slug=graphene.String())
@@ -44,11 +127,11 @@ class HomeQuery(object):
 
         if id is not None:
             parent_page = HomePage.objects.get(id=id)
-            return HomePage.objects.child_of(parent_page).get()
+            return HomePage.objects.child_of(parent_page)
 
         if slug is not None:
             parent_page = HomePage.objects.get(slug=slug)
-            return HomePage.objects.child_of(parent_page).get()
+            return HomePage.objects.child_of(parent_page)
 
         return None
 
--- home/models.py
+++ home/models.py
@@ -1,13 +1,71 @@
 from django.db import models
 
+from wagtail.core import blocks
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.snippets.blocks import SnippetChooserBlock
+from wagtail.snippets.models import register_snippet
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+
+
+@register_snippet
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    ingredients = StreamField([
+        ('ingredient', blocks.StructBlock([
+            ('name', blocks.CharBlock()),
+            ('quantity', blocks.DecimalBlock()),
+            ('unit', blocks.ChoiceBlock(choices=[
+                ('none', '(no unit)'),
+                ('g', 'Grams (g)'),
+                ('ml', 'Millilitre (ml)'),
+                ('tsp', 'Teaspoon (tsp.)'),
+                ('tbsp', 'Tablespoon (tbsp.)'),
+            ]))
+        ]))
+    ])
+    instructions = StreamField([
+        ('instruction', blocks.TextBlock()),
+    ])
+
+    panels = [
+        FieldPanel('title'),
+        ImageChooserPanel('image'),
+        StreamFieldPanel('ingredients'),
+        StreamFieldPanel('instructions'),
+    ]
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title
 
 
 class HomePage(Page):
     body = RichTextField(blank=True)
+    stream = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('recipe', SnippetChooserBlock(Recipe)),
+    ], blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
+        StreamFieldPanel('stream')
+    ]
+
+
+class TestPage(Page):
+    test = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('test', classname="full"),
     ]
